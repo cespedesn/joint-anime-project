dbd2416d20c62fe6f1122b7928c106f1ec448b57
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-10-07 13:58:33 -0400

    Final-update

--- src/Components/MangaSearchBar.js
+++ src/Components/MangaSearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MangaSearchBar({setMangaList, mangaSearchTerm, setMangaSearchTerm}) {
  
     function handleMangaSearchSubmit(e) {
-        e.preventDefault();
+        e?.preventDefault();
         fetch(`https://api.jikan.moe/v4/manga?q=${mangaSearchTerm}`)
         .then(res => res.json())
         .then(data => {
